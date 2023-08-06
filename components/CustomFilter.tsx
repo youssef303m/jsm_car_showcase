90389918a05e0546afd1fa9366dbc08387245536
Youssef303M <youssef303m@gmail.com>
2023-08-06 03:16:39 +0300

    Car Catalogue (finished search by manufacturer)

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomFilter = ({ title }) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,6 @@ export { default as Hero } from "./Hero";
 export { default as CustomButton } from "./CustomButton";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
+export { default as SearchBar } from "./SearchBar";
+export { default as CustomFilter } from "./CustomFilter";
+export { default as SearchManufacturer } from "./SearchManufacturer";
