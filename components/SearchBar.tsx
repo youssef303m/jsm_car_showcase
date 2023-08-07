3e8443fcc29815544982ed9b829970d70b19ae76
Youssef303M <youssef303m@gmail.com>
2023-08-08 02:59:50 +0300

    Converting to CSR to fix the scroll to top bug in Next.js

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { SearchManufacturer } from ".";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -16,62 +15,25 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   </button>
 );
 
-const SarchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
-  const [model, setModel] = useState("");
-  const router = useRouter();
-  const searchParams = new URLSearchParams(window.location.search);
-
-  useEffect(() => {
-    // Retrieve scrollY value from localStorage after routing
-    const persistentScroll = localStorage.getItem("persistentScroll");
-    if (persistentScroll === null) return;
-
-    // Restore the window's scroll position
-    window.scrollTo({ top: Number(persistentScroll) });
-
-    // Remove scrollY from localStorage after restoring the scroll position
-    // This hook will run before and after routing happens so this check is
-    // here to make we don't delete scrollY before routing
-    if (Number(persistentScroll) === window.scrollY)
-      localStorage.removeItem("persistentScroll");
-  }, [searchParams]);
+const SarchBar = ({ setManufacturer, setModel }) => {
+  const [searchManufacturer, setSearchManufacturer] = useState("");
+  const [searchModel, setSearchModel] = useState("");
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (manufacturer === "" && model === "") {
+    if (searchManufacturer === "" && searchModel === "") {
       return alert("Please fill in the search bar");
     }
-    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
-  };
-
-  const updateSearchParams = (model: string, manufacturer: string) => {
-    if (model) {
-      searchParams.set("model", model);
-    } else {
-      searchParams.delete("model");
-    }
-    if (manufacturer) {
-      searchParams.set("manufacturer", manufacturer);
-    } else {
-      searchParams.delete("manufacturer");
-    }
-    const newPathname = `${
-      window.location.pathname
-    }?${searchParams.toString()}`;
-
-    // Save current scrollY value to localStorage before pushing the new route
-    localStorage.setItem("persistentScroll", window.scrollY.toString());
-
-    router.push(newPathname);
+    setModel(searchModel);
+    setManufacturer(searchManufacturer);
   };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer
-          manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          selected={searchManufacturer}
+          setSelected={setSearchManufacturer}
         />
       </div>
       <div className="searchbar__item">
@@ -85,8 +47,8 @@ const SarchBar = () => {
         <input
           type="text"
           name="model"
-          value={model}
-          onChange={(e) => setModel(e.target.value)}
+          value={searchModel}
+          onChange={(e) => setSearchModel(e.target.value)}
           placeholder="Tiguan"
           className="searchbar__input"
         />
--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,12 +5,10 @@ import { useRouter } from "next/navigation";
 import { CustomButton } from ".";
 import { updateSearchParams } from "@/utils";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-  const router = useRouter();
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathname = updateSearchParams("limit", `${newLimit}`);
-    router.push(newPathname);
+    setLimit(newLimit);
   };
   return (
     <div className="w-full flex-center gap-5 mt-10">
